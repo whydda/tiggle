8d5022c3126b45fef3d36259da58144c9ecaf56b
whydda <whydda@naver.com>
2019-07-15 15:44:06 +0900

    vue.js 추가

--- src/main/java/kr/co/tiggle/security/SecurityConfig.java
+++ src/main/java/kr/co/tiggle/security/SecurityConfig.java
@@ -1,19 +1,23 @@
 package kr.co.tiggle.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/css/**, /static/js/**, *.ico");		// Resources 파일이나 Javascript 파일 경로 무시
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll();
